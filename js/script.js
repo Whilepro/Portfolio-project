310c5edc9aa4d52fd1d340ac2930549e63477bd9
Whilepro <rokas.nasickas@gmail.com>
2024-03-09 15:54:59 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Get the button:
+let myimage = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    myimage.style.display = "block";
+  } else {
+    myimage.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
